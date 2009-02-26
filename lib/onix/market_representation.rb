ee37ead8cb5f00cad707f80aed336cec692e6ba1
James Healy <jimmy@deefa.com>
2009-02-27 03:41:30 +1100

    added basic spec for MarketRepresentation class

--- lib/onix/market_representation.rb
+++ lib/onix/market_representation.rb
@@ -2,13 +2,15 @@ module ONIX
   class MarketRepresentation
     include ROXML
 
+    xml_name "MarketRepresentation"
+
     xml_accessor :agent_name, :from => "AgentName"
-    xml_accessor :agent_role, :from => "AgentRole", :as => Fixnum # should be a 2 digit num
+    xml_accessor :agent_role, :from => "AgentRole", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :market_country, :from => "MarketCountry"
     xml_accessor :market_territory, :from => "MarketTerritory"
     xml_accessor :market_country_excluded, :from => "MarketCountryExcluded"
     xml_accessor :market_restriction_detail, :from => "MarketRestrictionDetail"
-    xml_accessor :market_publishing_status, :from => "MarketPublishingStatus", :as => Fixnum # should be a 2 digit num
+    xml_accessor :market_publishing_status, :from => "MarketPublishingStatus", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
   end
 end
 
