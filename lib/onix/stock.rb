ed883011b99dfc885ecc25651f7b5117752319e0
James Healy <jimmy@deefa.com>
2009-02-27 20:55:32 +1100

    add basic spec for Stock class

--- lib/onix/stock.rb
+++ lib/onix/stock.rb
@@ -2,6 +2,8 @@ module ONIX
   class Stock
     include ROXML
 
+    xml_name "Stock"
+
     # NOTE: these *should* be numeric fields according to the spec,
     #       but heaps of ONIX files in the wild use text
     xml_accessor :on_hand, :from => "OnHand"
