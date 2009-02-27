11a729cf37ef957d3f279f4da6ec6e7869573092
James Healy <jimmy@deefa.com>
2009-02-27 21:25:22 +1100

    basic spec for SupplyDetail class

--- lib/onix/supply_detail.rb
+++ lib/onix/supply_detail.rb
@@ -2,6 +2,8 @@ module ONIX
   class SupplyDetail
     include ROXML
 
+    xml_name "SupplyDetail"
+
     xml_accessor :supplier_ean_location_number, :from => "SupplierEANLocationNumber"
     xml_accessor :supplier_san, :from => "SupplierSAN"
     xml_accessor :supplier_name, :from => "SupplierName"
@@ -9,11 +11,11 @@ module ONIX
     xml_accessor :fax_number, :from => "FaxNumber"
     xml_accessor :email_address, :from => "EmailAddress"
     xml_accessor :websites, :from => "Website", :as => [ONIX::Website]
-    xml_accessor :supplier_role, :from => "SupplierRole", :as => Fixnum # should be a 2 digit num
+    xml_accessor :supplier_role, :from => "SupplierRole", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :supply_to_country, :from => "SupplyToCountry"
     xml_accessor :supply_to_territory, :from => "SupplyToTerritory"
-    xml_accessor :availability_status_code, :from => "AvailabilityStatusCode", :as => Fixnum # should be a 2 digit num
-    xml_accessor :product_availability, :from => "ProductAvailability", :as => Fixnum # should be a 2 digit num
+    xml_accessor :availability_status_code, :from => "AvailabilityStatusCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
+    xml_accessor :product_availability, :from => "ProductAvailability", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :stock, :from => "Stock", :as => [ONIX::Stock]
     xml_accessor :prices, :from => "Price", :as => [ONIX::Price]
 
