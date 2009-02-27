b74e4d6c123d39fd8511474e598e00607519b50f
James Healy <jimmy@deefa.com>
2009-02-27 12:21:03 +1100

    added basic spec for MediaFile class

--- lib/onix/media_file.rb
+++ lib/onix/media_file.rb
@@ -2,10 +2,12 @@ module ONIX
   class MediaFile
     include ROXML
 
-    xml_accessor :media_file_type_code, :from => "MediaFileTypeCode", :as => Fixnum # should be a 2 digit num
-    xml_accessor :media_file_format_code, :from => "MediaFileFormatCode", :as => Fixnum # should be a 2 digit num
+    xml_name "MediaFile"
+
+    xml_accessor :media_file_type_code, :from => "MediaFileTypeCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
+    xml_accessor :media_file_format_code, :from => "MediaFileFormatCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :image_resolution, :from => "ImageResolution"
-    xml_accessor :media_file_link_type_code, :from => "MediaFileLinkTypeCode", :as => Fixnum # should be a 2 digit num
+    xml_accessor :media_file_link_type_code, :from => "MediaFileLinkTypeCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :media_file_link, :from => "MediaFileLink"
   end
 end
---
+++ spec/media_file_spec.rb
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+
+require 'onix'
+
+context "ONIX::MediaFile" do
+
+  before(:each) do
+    data_path = File.join(File.dirname(__FILE__),"..","data")
+    file1    = File.join(data_path, "media_file.xml")
+    @doc = LibXML::XML::Document.file(file1)
+    @root = @doc.root
+  end
+
+  specify "should correctly convert to a string" do
+    mf = ONIX::MediaFile.from_xml(@root.to_s)
+    mf.to_xml.to_s[0,11].should eql("<MediaFile>")
+  end
+
+  specify "should provide read access to first level attibutes" do
+    mf = ONIX::MediaFile.from_xml(@root.to_s)
+    mf.media_file_type_code.should eql(4)
+    mf.media_file_link_type_code.should eql(1)
+    mf.media_file_link.should eql("http://www.allenandunwin.com/BookCovers/resized_9788888028729_224_297_FitSquare.jpg")
+  end
+
+  specify "should provide write access to first level attibutes" do
+    mf = ONIX::MediaFile.new
+
+    mf.media_file_type_code = 2
+    mf.to_xml.to_s.include?("<MediaFileTypeCode>02</MediaFileTypeCode>").should be_true
+
+    mf.media_file_link_type_code = 1
+    mf.to_xml.to_s.include?("<MediaFileLinkTypeCode>01</MediaFileLinkTypeCode>").should be_true
+
+    mf.media_file_link = "http://www.google.com"
+    mf.to_xml.to_s.include?("<MediaFileLink>http://www.google.com</MediaFileLink>").should be_true
+  end
+
+end
+
