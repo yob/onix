133fab7e230d66e54cf38c687c06b7ae816757c1
James Healy <jimmy@deefa.com>
2009-02-27 19:27:03 +1100

    basic specs for ProductIdentifier class

--- lib/onix/product_identifier.rb
+++ lib/onix/product_identifier.rb
@@ -2,7 +2,9 @@ module ONIX
   class ProductIdentifier
     include ROXML
 
-    xml_accessor :product_id_type, :from => "ProductIDType", :as => Fixnum # should be a 2 digit num
+    xml_name "ProductIdentifier"
+
+    xml_accessor :product_id_type, :from => "ProductIDType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :id_value, :from => "IDValue"
   end
 end
