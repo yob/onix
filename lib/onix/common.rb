5b8bc434f00993f8ebb1ca9c1b1775ed989931d7
James Healy <jimmy@deefa.com>
2009-06-04 18:59:07 +1000

    comments

--- lib/onix/common.rb
+++ lib/onix/common.rb
@@ -1,5 +1,18 @@
 module ONIX
+  # methods we want to be available on all our ROXML classes
   module Common
+
+    # After an input string has been marshalled into an object,
+    # ROXML will automatically call this method.
+    #
+    # LibXML (and therefore ROXML) appears to forget the encoding
+    # of the input data, a damn shame considering the Ruby 1.9 encoding
+    # support can be vey useful.
+    #
+    # Since we only ever pass our ROXML objects strings we know are utf-8
+    # encoded, it should be safe enough to force every string attribute
+    # back once the marshalling is done.
+    #
     def after_parse
       self.class.roxml_attrs.each do |attr|
         var = instance_variable_get("@#{attr.variable_name}")
