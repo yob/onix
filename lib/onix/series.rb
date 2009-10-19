a0e8e1a34f700dd07afc8ad552ac376682ee2aa5
James Healy <jimmy@deefa.com>
2009-10-19 15:39:24 +1100

    Remove some methods from ONIX::Series - At this stage I prefer the ROXML classes to be "dumb containers"

--- lib/onix/series.rb
+++ lib/onix/series.rb
@@ -14,44 +14,5 @@ module ONIX
       self.series_identifiers = []
     end
 
-    # retrieve the proprietary series ID
-    def proprietary_series_id
-      series_identifier(1).andand.id_value
-    end
-
-    # set a new proprietary series ID
-    def proprietary_series_id=(id)
-      series_identifier_set(1, id)
-    end
-
-    # retrieve the issn
-    def issn
-      series_identifier(2).andand.id_value
-    end
-
-    # set a new issn
-    def issn=(id)
-      series_identifier_set(2, id)
-    end
-
-    # retrieve the value of a particular ID
-    def series_identifier(type)
-      identifier = series_identifiers.find { |id| id.series_id_type == type }
-    end
-
-    # set the value of a particular ID
-    def series_identifier_set(type, value)
-      series_indentifier_id = series_identifiers.find { |id| id.series_id_type == type }
-
-      # create a new series identifier record if we need to
-      if series_indentifier_id.nil?
-        series_indentifier_id = ONIX::SeriesIdentifier.new
-      end
-
-      series_indentifier_id.series_id_type = type
-      series_indentifier_id.id_value = value
-
-      series_identifiers << series_indentifier_id
-    end
   end
 end
