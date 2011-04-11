9ad5493bec39d09fd7e6c0174d2ab3e600cc2158
Joseph Pearson <joseph@inventivelabs.com.au>
2011-04-11 20:48:10 +1000

    Fleshing out the entire Product accessor suite for ONIX 2.1.

--- lib/onix/elements/product.rb
+++ lib/onix/elements/product.rb
@@ -5,54 +5,168 @@ module ONIX
 
     xml_name "Product"
 
+    # PR.1 Record reference number, type and source
     xml_accessor :record_reference, :from => "RecordReference"
-    xml_accessor :notification_type, :from => "NotificationType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :product_identifiers, :from => "ProductIdentifier", :as => [ONIX::ProductIdentifier]
-    #xml_accessor :product_form, :from => "ProductForm"
-    onix_code_from_list(:product_form, "ProductForm", :list => 7)
+    onix_code_from_list :notification_type, "NotificationType", :list => 1
+    onix_code_from_list :deletion_code, "DeletionCode", :list => 2
+    xml_accessor :deletion_text, :from => "DeletionText"
+    onix_code_from_list :record_source_type, "RecordSourceType", :list => 3
+    # FIXME: List 4 is missing?
+    # onix_code_from_list :record_source_identifier_type, "RecordSourceIdentifierType", :list => 4
+    xml_accessor :record_source_identifier, :from => "RecordSourceIdentifier"
+    xml_accessor :record_source_name, :from => "RecordSourceName"
 
-    xml_accessor :product_form_detail, :from => "ProductFormDetail"
-    xml_accessor :series, :from => "Series", :as => [ONIX::Series]
-    xml_accessor :titles, :from => "Title", :as => [ONIX::Title]
-    xml_accessor :websites, :from => "Website", :as => [ONIX::Website]
-    xml_accessor :contributors, :from => "Contributor", :as => [ONIX::Contributor]
+    # PR.2 Product numbers
+    onix_composite :product_identifiers, ONIX::ProductIdentifier
+    onix_codes_from_list :barcodes, "Barcode", :list => 6
+
+    # PR.3 Product Form
+    onix_code_from_list :product_form, "ProductForm", :list => 7
+    onix_codes_from_list :product_form_details, "ProductFormDetail", :list => 78
+    # TODO: onix_composite :product_form_features, ONIX::ProductFormFeature
+    onix_code_from_list :product_packaging, "ProductPackaging", :list => 80
+    xml_accessor :product_form_description, :from => "ProductFormDescription"
+    xml_accessor :number_of_pieces, :from => "NumberOfPieces", :as => Fixnum
+    onix_code_from_list :trade_category, "TradeCategory", :list => 12
+    onix_code_from_list :product_content_type, "ProductContentType", :list => 81
+
+    # TODO: onix_composite :contained_items, ONIX::ContainedItem
+    # TODO: onix_composite :product_classifications, ONIX::ProductClassification
+
+    # PR.4 Epublication
+    onix_code_from_list :epub_type, "EpubType", :list => 10
+    xml_accessor :epub_type_version, "EpubTypeVersion"
+    xml_accessor :epub_type_description, "EpubTypeDescription"
+    onix_code_from_list :epub_format, "EpubFormat", :list => 11
+    xml_accessor :epub_format_version, "EpubFormatVersion"
+    xml_accessor :epub_format_description, "EpubFormatDescription"
+    onix_code_from_list :epub_source, "EpubSource", :list => 11
+    xml_accessor :epub_source_version, "EpubSourceVersion"
+    xml_accessor :epub_source_description, "EpubSourceDescription"
+    xml_accessor :epub_type_note, "EpubTypeNote"
+
+    onix_composite :series, ONIX::Series
+
+    # PR.6 Set
+    # TODO: onix_composite :sets, ONIX::Set
+
+    # PR.7 Title
+    onix_composite :titles, ONIX::Title
+    # TODO: onix_composite :work_identifiers, ONIX::WorkIdentifier
+    onix_composite :websites, ONIX::Website
+    onix_code_from_list :thesis_type, "ThesisType", :list => 72
+    xml_accessor :thesis_presented_to, "ThesisPresentedTo"
+    xml_accessor :year_of_thesis, "YearOfThesis", :as => Fixnum
+
+    # PR.8 Authorship
+    onix_composite :contributors, ONIX::Contributor
+
+    # PR.9 Conference
+    # TODO: onix_composite :conferences, ONIX::Conference
+
+    # PR.10 Edition
+    onix_code_from_list :edition_type_code, "EditionTypeCode", :list => 21
     xml_accessor :edition_number, :from => "EditionNumber", :as => Fixnum
-    xml_accessor :languages, :from => "Language", :as => [ONIX::Language]
+    xml_accessor :edition_version_number, :from => "EditionVersionNumber"
+    xml_accessor :edition_statement, :from => "EditionStatement"
+    # FIXME: NoEdition is an empty element flag. How to capture this?
+    # TODO: onix_composite :religious_text, ONIX::ReligiousText, :singular => true
+
+    # PR.11 Language
+    onix_composite :languages, ONIX::Language
+
+    # PR.12 Extents and other content
     xml_accessor :number_of_pages, :from => "NumberOfPages", :as => Fixnum
+    xml_accessor :pages_roman, :from => "PagesRoman"
+    xml_accessor :pages_arabic, :from => "PagesArabic", :as => Fixnum
+    # TODO: onix_composite :extents, ONIX::Extent
+    xml_accessor :number_of_illustrations, :from => "NumberOfIllustrations", :as => Fixnum
+    xml_accessor :illustrations_note, :from => "IllustrationsNote"
+    # TODO: onix_composite :illustrations, ONIX::Illustrations
+    xml_accessor :map_scale, :from => "MapScale", :as => [Fixnum]
+
+    # PR.13 Subject
     xml_accessor :basic_main_subject, :from => "BASICMainSubject"
     xml_accessor :bic_main_subject, :from => "BICMainSubject"
-    xml_accessor :subjects, :from => "Subject", :as => [ONIX::Subject]
-    xml_accessor :audience_code, :from => "AudienceCode", :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :audience_ranges, :from => "AudienceRange", :as => [ONIX::AudienceRange]
-    xml_accessor :text, :from => "OtherText", :as => [ONIX::OtherText]
-    xml_accessor :media_files, :from => "MediaFile", :as => [ONIX::MediaFile]
-    xml_accessor :imprints, :from => "Imprint", :as => [ONIX::Imprint]
-    xml_accessor :publishers, :from => "Publisher", :as => [ONIX::Publisher]
-    xml_accessor :publishing_status, :from => "PublishingStatus", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor(:publication_date, :from => "PublicationDate", :to_xml => ONIX::Formatters.yyyymmdd) do |val|
-      begin
-        Date.parse(val)
-      rescue
-        nil
-      end
-    end
-    xml_accessor :copyright_year, :from => "CopyrightYear", :as => Integer
-    xml_accessor :year_first_published, :from => "YearFirstPublished", :as => Fixnum
-    xml_accessor :sales_restrictions, :from => "SalesRestriction", :as => [ONIX::SalesRestriction]
-    xml_accessor :measurements, :from => "Measure", :as => [ONIX::Measure]
-    xml_accessor :supply_details, :from => "SupplyDetail", :as => [ONIX::SupplyDetail]
-    xml_accessor :market_representations, :from => "MarketRepresentation", :as => [ONIX::MarketRepresentation]
-    xml_accessor :sales_rights, :from => "SalesRights", :as => [ONIX::SalesRights]
-    xml_accessor :not_for_sales, :from => "NotForSale", :as => [ONIX::NotForSale]
+    # TODO: onix_composite :main_subject, ONIX::MainSubject
+    onix_composite :subjects, ONIX::Subject
+    onix_composite :person_as_subjects, ONIX::Contributor, :from => "PersonAsSubject"
+    xml_accessor :corporate_body_as_subject, :from => "CorporateBodyAsSubject", :as => []
+    xml_accessor :place_as_subject, :from => "PlaceAsSubject", :as => []
 
-    # some deprecated attributes. Read only
-    # - See the measures array for the current way of specifying
+    # PR.14 Audience
+    onix_codes_from_list :audience_codes, "AudienceCode", :list => 28
+    # TODO: onix_composite :audiences, ONIX::Audience
+    onix_composite :audience_ranges, ONIX::AudienceRange
+    xml_accessor :audience_description, :from => "AudienceDescription"
+    # TODO: onix_composite :complexities, ONIX::Complexity
+
+    # PR.15 Descriptions and supporting texts
+    # FIXME: should this be :text, :texts or :other_texts? Can we alias?
+    onix_composite :text, ONIX::OtherText
+
+    # PR.16 Links to image/audio/video files
+    onix_composite :media_files, ONIX::MediaFile
+    onix_composite :product_websites, ONIX::Website, :from => "ProductWebsite"
+
+    # PR.17 Prizes
+    # TODO: onix_composite :prizes, ONIX::Prize
+
+    # PR.18 Content items
+    # TODO: onix_composite :content_items, ONIX::ContentItem
+
+    # PR.19 Publisher
+    onix_composite :imprints, ONIX::Imprint
+    onix_composite :publishers, ONIX::Publisher
+    xml_accessor :cities_of_publication, :from => "CityOfPublication", :as => []
+    onix_code_from_list :country_of_publication, "CountryOfPublication", :list => 91
+
+    # PR.20 Publishing status and dates, and copyright
+    onix_code_from_list :publishing_status, "PublishingStatus", :list => 64
+    xml_accessor :publishing_status_note, :from => "PublishingStatusNote"
+    onix_date_accessor :announcement_date, "AnnouncementDate"
+    onix_date_accessor :trade_announcement_date, "TradeAnnouncementDate"
+    onix_date_accessor :publication_date, "PublicationDate"
+    # TODO: onix_composite :copyright_statements, ONIX::CopyrightStatement
+    xml_accessor :copyright_year, :from => "CopyrightYear", :as => Fixnum
+    xml_accessor :year_first_published, :from => "YearFirstPublished", :as => Fixnum
+
+    # PR.21 Territorial rights and other sales restrictions
+    onix_composite :sales_rights, ONIX::SalesRights
+    onix_composite :not_for_sales, ONIX::NotForSale
+    onix_composite :sales_restrictions, ONIX::SalesRestriction
+
+    # PR.22 Dimensions
+    # FIXME: rename to :measures?
+    onix_composite :measurements, ONIX::Measure
+    # Some deprecated attributes. Read only
+    # - See the measurements array for the current way of specifying
     #   various measurements of the product
     xml_reader :height,     :from => "Height", :as => BigDecimal
     xml_reader :width,      :from => "Width", :as => BigDecimal
     xml_reader :thickness,  :from => "Thickness", :as => BigDecimal
     xml_reader :weight,     :from => "Weight", :as => BigDecimal
     xml_reader :dimensions, :from => "Dimensions"
+
+    # PR.23 Related products
+    onix_composite :related_products, ONIX::Product, :from => "RelatedProduct"
+    onix_code_from_list :relation_code, "RelationCode", :list => 51
+    onix_date_accessor :out_of_print_date, "OutOfPrintDate"
+
+    # PR.24 Supplier, availability and prices
+    onix_composite :supply_details, ONIX::SupplyDetail
+
+    # PR.25 Market representation
+    onix_composite :market_representations, ONIX::MarketRepresentation
+
+    # PR.26 Sales promotion information
+    xml_accessor :promotion_campaign, :from => "PromotionCampaign"
+    xml_accessor :promotion_contact_details, :from => "PromotionContactDetails"
+    xml_accessor :initial_print_run, :from => "InitialPrintRun"
+    xml_accessor :reprint_details, :from => "ReprintDetail", :as => []
+    xml_accessor :copies_sold, :from => "CopiesSold"
+    xml_accessor :book_club_adoption, :from => "BookClubAdoption"
+
 
     def initialize
       self.product_identifiers = []
