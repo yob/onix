477196979fa62d7ae1d68af4a6200fb8d3922700
Joseph Pearson <joseph@inventivelabs.com.au>
2011-04-11 10:57:32 +1000

    Reader includes an option not to load the DTD.
    Documentation provides a warning, but it might be a useful flag in specs
    and simple scripts with known-good data.

--- lib/onix/reader.rb
+++ lib/onix/reader.rb
@@ -44,15 +44,27 @@ module ONIX
   #   puts reader.xml_lang
   #   puts reader.xml_version
   #
+  # Note that ONIX has 1500 valid named entities (such as &ndash;) that can
+  # cause the parser to throw an exception because it doesn't recognise them.
+  # To work around this, the parser loads the ONIX DTD. It generally has to
+  # do this over the internet, which slows parsing considerably. To skip DTD
+  # loading (running the risk of parser exceptions), you can pass an option
+  # to the constructor:
+  #
+  #   reader = ONIX::Reader.new("somefile.xml", ONIX::Product, :dtd => false)
+  #
+  # For more information, see http://is.gd/p7fHQq
+  #
   class Reader
     include Enumerable
 
     attr_reader :header, :release
     attr_reader :xml_lang, :xml_version
 
-    def initialize(input, product_klass = ::ONIX::Product)
+    def initialize(input, product_klass = ::ONIX::Product, options = {})
       @input = input
       @product_klass = product_klass
+      @options = options || {}
 
       create_parser
 
@@ -98,11 +110,15 @@ module ONIX
     private
 
     def create_parser
+      parser_config = lambda { |cfg|
+        cfg.noent
+        cfg.dtdload  unless @options[:dtd] == false
+      }
       if @input.kind_of?(String)
         @file   = File.open(@input, "r")
-        @reader = Nokogiri::XML::Reader(@file)# { |cfg| cfg.dtdload.noent }
+        @reader = Nokogiri::XML::Reader(@file, &parser_config)
       elsif @input.kind_of?(IO)
-        @reader = Nokogiri::XML::Reader(@input)# { |cfg| cfg.dtdload.noent }
+        @reader = Nokogiri::XML::Reader(@input, &parser_config)
       else
         raise ArgumentError, "Unable to read from file or IO stream"
       end
