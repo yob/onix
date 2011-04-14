5622733705abbc5fee21127019c299304167f24e
James Healy <jimmy@deefa.com>
2011-04-14 11:59:38 +1000

    remove earlier encoding changes to ONIX::Normaliser
    * much easier to just tell Nokogiri::XML::Reader what encoding
      the source is instead of rewriting the entire file

--- lib/onix/normaliser.rb
+++ lib/onix/normaliser.rb
@@ -11,24 +11,18 @@ module ONIX
   #
   # This will create a new file that:
   #
+  # - is UTF-8 encoded
   # - uses reference tags, not short
-  # - doesn't contain control characters banned by the XML spec
-  #
-  # Optionally, it can also rewrite the XML declaration with a new
-  # encoding. This is useful when the original generator of the file left
-  # the encoding indicator off or got it wrong. The encoding indicator of
-  # the file *MUST* be correct for ONIX::Reader to function correctly.
+  # - has no named entities (ndash, etc) other than &amp; &lt; and &gt;
   #
   # Usage:
   #
   #   ONIX::Normaliser.process("oldfile.xml", "newfile.xml")
-  #   ONIX::Normaliser.process("oldfile.xml", "newfile.xml", :encoding => "iso-8859-1")
-  #   ONIX::Normaliser.process("oldfile.xml", "newfile.xml", :encoding => "utf-8")
-  #
+  #   
   # Dependencies:
   #
   # At this stage the class depends on several external apps, all commonly available
-  # on *nix systems: xsltproc, tr and sed
+  # on *nix systems: xsltproc, isutf8, iconv and sed
   #
   class Normaliser
 
@@ -37,20 +31,17 @@ module ONIX
       # normalise oldfile and save it as newfile. oldfile
       # will be left untouched
       #
-      def process(oldfile, newfile, opts = {})
-        self.new(oldfile, newfile, opts).run
+      def process(oldfile, newfile)
+        self.new(oldfile, newfile).run
       end
     end
 
-    def initialize(oldfile, newfile, opts = {})
+    def initialize(oldfile, newfile)
       raise ArgumentError, "#{oldfile} does not exist" unless File.file?(oldfile)
       raise ArgumentError, "#{newfile} already exists" if File.file?(newfile)
       raise "xsltproc app not found" unless app_available?("xsltproc")
       raise "tr app not found"       unless app_available?("tr")
-      raise "sed app not found"      unless app_available?("sed")
-      raise "cat app not found"      unless app_available?("cat")
 
-      @options = opts
       @oldfile = oldfile
       @newfile = newfile
       @curfile = next_tempfile
@@ -63,12 +54,6 @@ module ONIX
       if @head.include?("ONIXmessage")
         dest = next_tempfile
         to_reference_tags(@curfile, dest)
-        @curfile = dest
-      end
-
-      if @options[:encoding]
-        dest = next_tempfile
-        fix_encoding_mark(@curfile, dest, @options[:encoding])
         @curfile = dest
       end
 
@@ -97,20 +82,6 @@ module ONIX
         tf.close!
       end
       p
-    end
-
-    def fix_encoding_mark(src, dest, enc)
-      inpath = File.expand_path(src)
-      outpath = File.expand_path(dest)
-
-      head = File.open(inpath,"r").read(512)
-      decl = "<?xml version=\"1.0\" encoding=\"#{enc}\" ?>"
-      if head.include?("<?xml")
-        `cat #{inpath} | sed -e 's/xml[^\\?]\\+/xml version="1.0" encoding="iso-8859-1" /' > #{outpath}`
-      else
-        `echo '#{decl}' > #{outpath}`
-        `cat #{inpath} >> #{outpath}`
-      end
     end
 
     # uses an XSLT stylesheet provided by edituer to convert
