2be3f4f3e81871f54425682ea827c855222138d9
Joseph Pearson <joseph@inventivelabs.com.au>
2012-05-16 12:57:10 +1000

    Typo in DiscountCoded.

--- lib/onix/elements/discount_coded.rb
+++ lib/onix/elements/discount_coded.rb
@@ -4,5 +4,5 @@ class ONIX::DiscountCoded < ONIX::Element
   xml_name "DiscountCoded"
   onix_code_from_list :discount_code_type, "DiscountCodeType", :list => 100
   xml_accessor :discount_code_type_name, :from => "DiscountCodeTypeName"
-  xml_accessor :discount_value
+  xml_accessor :discount_code
 end
