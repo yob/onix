fa33770af8d28de33df8d9d87bd5e83ebfac18e9
honzor <johannes.martin@es-tech.eu>
2014-03-28 15:37:20 +0100

    added IDTypeName to the product identifier model

--- lib/onix/product_identifier.rb
+++ lib/onix/product_identifier.rb
@@ -8,5 +8,6 @@ module ONIX
 
     xml_accessor :product_id_type, :from => "ProductIDType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :id_value, :from => "IDValue"
+    xml_accessor :id_type_name, :from => "IDTypeName"
   end
 end
