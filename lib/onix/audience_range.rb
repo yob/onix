dc02c8d41bad4dddfb263ca1f30b82711bac7b9e
Aarti Parikh <aarti@sachmanya.com>
2014-04-24 06:39:37 -0700

    Multiple changes
    Replaced Source Gemfile
    Updated deprecated rake/Rdoc task in Rakefile
    Fixed Audience_range_spec.rb
    Added correct order for Audience_range XML

--- lib/onix/audience_range.rb
+++ lib/onix/audience_range.rb
@@ -7,19 +7,34 @@ module ONIX
     xml_name "AudienceRange"
 
     xml_accessor :audience_range_qualifier, :from => "AudienceRangeQualifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :audience_range_precisions, :from => "AudienceRangePrecision", :as => [Fixnum], :to_xml => [ONIX::Formatters.two_digit] # TODO: two_digit isn't working on the array items
-    xml_accessor :audience_range_values, :from => "AudienceRangeValue"
-
-    # TODO: element AudienceRange: validity error :
-    #   Element AudienceRange content does not follow the DTD, expecting
-    #   (AudienceRangeQualifier , AudienceRangePrecision , AudienceRangeValue ,
-    #   (AudienceRangePrecision , AudienceRangeValue)?),
-    #   got
-    #   (AudienceRangeQualifier AudienceRangePrecision AudienceRangePrecision
-    #   AudienceRangeValue AudienceRangeValue )
+    xml_accessor :audience_range_precisions, :from => "AudienceRangePrecision", :as => [Fixnum] , :to_xml => [ONIX::Formatters.two_digit]
+    xml_accessor :audience_range_values, :from => "AudienceRangeValue", :as => [Fixnum] , :to_xml => [ONIX::Formatters.two_digit]
+    
     def initialize
       self.audience_range_precisions = []
       self.audience_range_values = []
     end
+
+    # Returns an XML object representing this object
+    def to_xml(params = {})
+      params.reverse_merge!(:name => self.class.tag_name, :namespace => self.class.roxml_namespace)
+      params[:namespace] = nil if ['*', 'xmlns'].include?(params[:namespace])
+      XML.new_node([params[:namespace], params[:name]].compact.join(':')).tap do |root|
+        refs =  (self.roxml_references.present? \
+                 ? self.roxml_references \
+                 : self.class.roxml_attrs.map {|attr| attr.to_ref(self) })
+
+
+        refs[0].update_xml(root, refs[0].to_xml(self))  #:audience_range_qualifier
+
+        if refs[1] #:audience_range_precisions
+          refs[1].to_xml(self).each_with_index do |r, i|
+            refs[1].update_xml(root, [refs[1].to_xml(self)[i]] )
+            refs[2].update_xml(root, [refs[2].to_xml(self)[i]] )
+          end
+        end
+      end
+    end
   end
+
 end
