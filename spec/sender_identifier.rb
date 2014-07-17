4fa44156ba601afec9c7f9dd9a169fc2eb01dccf
Patri <patrimglez@gmail.com>
2014-07-17 14:06:46 +0200

    update 'sender_identifier' spec to use Given syntax

--- spec/sender_identifier.rb
+++ spec/sender_identifier.rb
@@ -4,34 +4,30 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe ONIX::SenderIdentifier do
 
-  before(:each) do
+  Given{
     data_path = File.join(File.dirname(__FILE__),"..","data")
     file1    = File.join(data_path, "sender_identifier.xml")
     @doc     = Nokogiri::XML::Document.parse(File.read(file1))
     @root = @doc.root
+  }
+
+  context "should correctly convert to a string" do
+    Given(:id){ONIX::SenderIdentifier.from_xml(@root.to_s)}
+    Then { id.to_xml.to_s[0,18] == "<SenderIdentifier>" }
   end
 
-  it "should correctly convert to a string" do
-    id = ONIX::SenderIdentifier.from_xml(@root.to_s)
-    id.to_xml.to_s[0,18].should eql("<SenderIdentifier>")
+  context "should provide read access to first level attributes" do
+    Given(:id){ONIX::SenderIdentifier.from_xml(@root.to_s)}
+    Then{ id.sender_id_type == 1 }
+    And{ id.id_value == "123456" }
   end
 
-  it "should provide read access to first level attributes" do
-    id = ONIX::SenderIdentifier.from_xml(@root.to_s)
-
-    id.sender_id_type.should eql(1)
-    id.id_value.should eql("123456")
-  end
-
-  it "should provide write access to first level attributes" do
-    id = ONIX::SenderIdentifier.new
-
-    id.sender_id_type = 1
-    id.to_xml.to_s.include?("<SenderIDType>01</SenderIDType>").should be_true
-
-    id.id_value = "54321"
-    id.to_xml.to_s.include?("<IDValue>54321</IDValue>").should be_true
-
+  context "should provide write access to first level attributes" do
+    Given(:id){ ONIX::SenderIdentifier.new }
+    When{ id.sender_id_type = 1 }
+    When{ id.id_value = "54321" }
+    Then{ id.to_xml.to_s.include?("<SenderIDType>01</SenderIDType>") == true }
+    And{ id.to_xml.to_s.include?("<IDValue>54321</IDValue>") == true }
   end
 
 end
