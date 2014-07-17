c3282a01faf51bd5b2fb69faa4fbc43943b93800
Patri <patrimglez@gmail.com>
2014-07-17 17:13:28 +0200

    'product_identifier_spec' using Give syntax

--- spec/product_identifier_spec.rb
+++ spec/product_identifier_spec.rb
@@ -4,33 +4,33 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe ONIX::ProductIdentifier do
 
-  before(:each) do
+  Given{
     data_path = File.join(File.dirname(__FILE__),"..","data")
     file1    = File.join(data_path, "product_identifier.xml")
     @doc     = Nokogiri::XML::Document.parse(File.read(file1))
     @root = @doc.root
+  }
+
+  context "should correctly convert to a string" do
+    Given(:id){ ONIX::ProductIdentifier.from_xml(@root.to_s) }
+    Then{ id.to_xml.to_s[0,19] == "<ProductIdentifier>" }
   end
 
-  it "should correctly convert to a string" do
-    id = ONIX::ProductIdentifier.from_xml(@root.to_s)
-    id.to_xml.to_s[0,19].should eql("<ProductIdentifier>")
+  context "should provide read access to first level attributes" do
+    Given(:id){ ONIX::ProductIdentifier.from_xml(@root.to_s) }
+
+    Then{ id.product_id_type == 2 }
+    And{ id.id_value == "0858198363" }
   end
 
-  it "should provide read access to first level attributes" do
-    id = ONIX::ProductIdentifier.from_xml(@root.to_s)
+  context "should provide write access to first level attributes" do
+    Given(:id){ ONIX::ProductIdentifier.new }
 
-    id.product_id_type.should eql(2)
-    id.id_value.should eql("0858198363")
-  end
+    When{ id.product_id_type = 2 }
+    When{ id.id_value = "James" }
 
-  it "should provide write access to first level attributes" do
-    id = ONIX::ProductIdentifier.new
-
-    id.product_id_type = 2
-    id.to_xml.to_s.include?("<ProductIDType>02</ProductIDType>").should be_true
-
-    id.id_value = "James"
-    id.to_xml.to_s.include?("<IDValue>James</IDValue>").should be_true
+    Then{ id.to_xml.to_s.include?("<ProductIDType>02</ProductIDType>") == true }
+    And{ id.to_xml.to_s.include?("<IDValue>James</IDValue>") == true }
 
   end
 
