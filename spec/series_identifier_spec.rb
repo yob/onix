1a6f191811d19e60f2f12a2abd21bf45e83bfaa1
Patri <patrimglez@gmail.com>
2014-07-17 17:19:06 +0200

    update 'series_identifier_spec'

--- spec/series_identifier_spec.rb
+++ spec/series_identifier_spec.rb
@@ -4,33 +4,33 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe ONIX::SeriesIdentifier do
 
-  before(:each) do
+  Given{
     data_path = File.join(File.dirname(__FILE__),"..","data")
     file1    = File.join(data_path, "series_identifier.xml")
     @doc = Nokogiri::XML::Document.parse(File.read(file1))
     @root = @doc.root
+  }
+
+  context "should correctly convert to a string" do
+    Given(:series){ ONIX::SeriesIdentifier.from_xml(@root.to_s) }
+    Then{ series.to_xml.to_s[0,18] == "<SeriesIdentifier>" }
   end
 
-  it "should correctly convert to a string" do
-    series = ONIX::SeriesIdentifier.from_xml(@root.to_s)
-    series.to_xml.to_s[0,18].should eql("<SeriesIdentifier>")
+  context "should provide read access to first level attributes" do
+    Given(:series){ ONIX::SeriesIdentifier.from_xml(@root.to_s) }
+
+    Then{ series.series_id_type == 1 }
+    And{ series.id_value == "10001" }
   end
 
-  it "should provide read access to first level attributes" do
-    series = ONIX::SeriesIdentifier.from_xml(@root.to_s)
+  context "should provide write access to first level attributes" do
+    Given(:series){ ONIX::SeriesIdentifier.new }
 
-    series.series_id_type.should eql(1)
-    series.id_value.should eql("10001")
-  end
+    When{ series.series_id_type = 9 }
+    When{ series.id_value = 999 }
 
-  it "should provide write access to first level attributes" do
-    series = ONIX::SeriesIdentifier.new
-
-    series.series_id_type = 9
-    series.to_xml.to_s.include?("<SeriesIDType>09</SeriesIDType>").should be_true
-
-    series.id_value = 999
-    series.to_xml.to_s.include?("<IDValue>999</IDValue>").should be_true
+    Then{ series.to_xml.to_s.include?("<SeriesIDType>09</SeriesIDType>") == true }
+    And{ series.to_xml.to_s.include?("<IDValue>999</IDValue>") == true }
   end
 
 end
