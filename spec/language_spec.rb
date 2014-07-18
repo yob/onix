2951567e816516bf1d37f1685a40644468927d4d
Patri <patrimglez@gmail.com>
2014-07-18 14:04:16 +0200

    Given specs for language

--- spec/language_spec.rb
+++ spec/language_spec.rb
@@ -4,37 +4,35 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe ONIX::Language do
 
-  before(:each) do
-    data_path = File.join(File.dirname(__FILE__),"..","data")
-    file1    = File.join(data_path, "language.xml")
-    @doc = Nokogiri::XML::Document.parse(File.read(file1))
-    @root = @doc.root
+  Given(:doc) { File.read(File.join(File.dirname(__FILE__), "..", "data", "language.xml")) }
+
+  describe "should correctly convert to a string" do
+    Given(:lan) { ONIX::Language.from_xml(doc) }
+    Then { lan.to_xml.to_s.start_with? "<Language>" }
   end
 
-  it "should correctly convert to a string" do
-    lan = ONIX::Language.from_xml(@root.to_s)
-    lan.to_xml.to_s[0,10].should eql("<Language>")
+  describe "should provide read access to first level attributes" do
+    Given(:lan) { ONIX::Language.from_xml(doc) }
+
+    Then { lan.language_role == 1 }
+    Then { lan.language_code == "eng" }
+    Then { lan.country_code == "US" }
   end
 
-  it "should provide read access to first level attributes" do
-    lan = ONIX::Language.from_xml(@root.to_s)
-
-    lan.language_role.should eql(1)
-    lan.language_code.should eql("eng")
-    lan.country_code.should eql("US")
-  end
-
-  it "should provide write access to first level attributes" do
-    lan = ONIX::Language.new
-
-    lan.language_role = 2
-    lan.to_xml.to_s.include?("<LanguageRole>02</LanguageRole>").should be_true
-
-    lan.language_code = "aar"
-    lan.to_xml.to_s.include?("<LanguageCode>aar</LanguageCode>").should be_true
-
-    lan.country_code = "AD"
-    lan.to_xml.to_s.include?("<CountryCode>AD</CountryCode>").should be_true
+  context "should provide write access to first level attributes" do
+    Given(:lan) { ONIX::Language.new }
+    describe :language_role= do
+      When { lan.language_role = 2 }
+      Then { lan.to_xml.to_s.include? "<LanguageRole>02</LanguageRole>" }
+    end
+    describe :language_code= do
+      When { lan.language_code = "aar" }
+      Then { lan.to_xml.to_s.include? "<LanguageCode>aar</LanguageCode>" }
+    end
+    describe :country_code= do
+      When { lan.country_code = "AD" }
+      Then { lan.to_xml.to_s.include? "<CountryCode>AD</CountryCode>" }
+    end
   end
 
 end
