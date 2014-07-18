dba53d1b2bcb30c3080419699c3fc2d8644e0493
Patri <patrimglez@gmail.com>
2014-07-18 16:24:41 +0200

    Given specs for media_file

--- spec/media_file_spec.rb
+++ spec/media_file_spec.rb
@@ -4,37 +4,35 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe ONIX::MediaFile do
 
-  before(:each) do
-    data_path = File.join(File.dirname(__FILE__),"..","data")
-    file1    = File.join(data_path, "media_file.xml")
-    @doc     = Nokogiri::XML::Document.parse(File.read(file1))
-    @root = @doc.root
+  Given(:doc) { File.read(File.join(File.dirname(__FILE__), "..", "data", "media_file.xml")) }
+
+  describe "should correctly convert to a string" do
+    Given(:mf) { ONIX::MediaFile.from_xml(doc) }
+    Then { mf.to_xml.to_s.start_with? "<MediaFile>" }
   end
 
-  it "should correctly convert to a string" do
-    mf = ONIX::MediaFile.from_xml(@root.to_s)
-    mf.to_xml.to_s[0,11].should eql("<MediaFile>")
+  describe "should provide read access to first level attributes" do
+    Given(:mf) { ONIX::MediaFile.from_xml(doc) }
+
+    Then { mf.media_file_type_code == 4 }
+    Then { mf.media_file_link_type_code == 1 }
+    Then { mf.media_file_link == "http://www.allenandunwin.com/BookCovers/resized_9788888028729_224_297_FitSquare.jpg" }
   end
 
-  it "should provide read access to first level attributes" do
-    mf = ONIX::MediaFile.from_xml(@root.to_s)
-    mf.media_file_type_code.should eql(4)
-    mf.media_file_link_type_code.should eql(1)
-    mf.media_file_link.should eql("http://www.allenandunwin.com/BookCovers/resized_9788888028729_224_297_FitSquare.jpg")
-  end
-
-  it "should provide write access to first level attributes" do
-    mf = ONIX::MediaFile.new
-
-    mf.media_file_type_code = 2
-    mf.to_xml.to_s.include?("<MediaFileTypeCode>02</MediaFileTypeCode>").should be_true
-
-    mf.media_file_link_type_code = 1
-    mf.to_xml.to_s.include?("<MediaFileLinkTypeCode>01</MediaFileLinkTypeCode>").should be_true
-
-    mf.media_file_link = "http://www.google.com"
-    mf.to_xml.to_s.include?("<MediaFileLink>http://www.google.com</MediaFileLink>").should be_true
+  context "should provide write access to first level attributes" do
+    Given(:mf) { ONIX::MediaFile.new }
+    describe :media_file_type_code= do
+      When { mf.media_file_type_code = 2 }
+      Then { mf.to_xml.to_s.include? "<MediaFileTypeCode>02</MediaFileTypeCode>" }
+    end
+    describe :media_file_link_type_code= do
+      When { mf.media_file_link_type_code = 1 }
+      Then { mf.to_xml.to_s.include? "<MediaFileLinkTypeCode>01</MediaFileLinkTypeCode>" }
+    end
+    describe :media_file_link= do
+      When { mf.media_file_link = "http://www.google.com" }
+      Then { mf.to_xml.to_s.include? "<MediaFileLink>http://www.google.com</MediaFileLink>" }
+    end
   end
 
 end
-
