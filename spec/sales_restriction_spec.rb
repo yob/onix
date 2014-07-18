5e5a492db96698ae68398861cd1f99b5cd9bc159
Patri <patrimglez@gmail.com>
2014-07-18 16:30:38 +0200

    Given specs for sales_restriction

--- spec/sales_restriction_spec.rb
+++ spec/sales_restriction_spec.rb
@@ -4,29 +4,24 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe "ONIX::SalesRestriction" do
 
-  before(:each) do
-    data_path = File.join(File.dirname(__FILE__),"..","data")
-    file1    = File.join(data_path, "sales_restriction.xml")
-    @doc     = Nokogiri::XML::Document.parse(File.read(file1))
-    @root = @doc.root
+  Given(:doc) { File.read(File.join(File.dirname(__FILE__), "..", "data", "sales_restriction.xml")) }
+
+  describe "should correctly convert to a string" do
+    Given(:sr) { ONIX::SalesRestriction.from_xml(doc) }
+    Then { sr.to_xml.to_s.start_with? "<SalesRestriction>" }
   end
 
-  it "should correctly convert to a string" do
-    sr = ONIX::SalesRestriction.from_xml(@root.to_s)
-    sr.to_xml.to_s[0,18].should eql("<SalesRestriction>")
+  describe "should provide read access to first level attributes" do
+    Given(:sr) { ONIX::SalesRestriction.from_xml(doc) }
+    Then { sr.sales_restriction_type == 0 }
   end
 
-  it "should provide read access to first level attributes" do
-    sr = ONIX::SalesRestriction.from_xml(@root.to_s)
-
-    sr.sales_restriction_type.should eql(0)
-  end
-
-  it "should provide write access to first level attributes" do
-    sr = ONIX::SalesRestriction.new
-
-    sr.sales_restriction_type = 1
-    sr.to_xml.to_s.include?("<SalesRestrictionType>01</SalesRestrictionType>").should be_true
+  context "should provide write access to first level attributes" do
+    Given(:sr) { ONIX::SalesRestriction.new }
+    describe :sales_restriction_type= do
+      When { sr.sales_restriction_type = 1 }
+      Then { sr.to_xml.to_s.include? "<SalesRestrictionType>01</SalesRestrictionType>" }
+    end
   end
 
 end
