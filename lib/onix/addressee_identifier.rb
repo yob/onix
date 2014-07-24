51b653f503cef6f613870ac518ec99e140a820d4
Patri <patrimglez@gmail.com>
2014-07-24 19:02:35 +0200

    fixes in addressee_identifier

--- lib/onix/addressee_identifier.rb
+++ lib/onix/addressee_identifier.rb
@@ -4,7 +4,9 @@ module ONIX
   class AddresseeIdentifier
     include ROXML
 
-    xml_accessor :addressee_id_type, :from => "AddresseeIDType", :as => Fixnum # should be a 2 digit num
+    xml_name "AddresseeIdentifier"
+
+    xml_accessor :addressee_id_type, :from => "AddresseeIDType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
     xml_accessor :id_type_name,      :from => "IDTypeName"
     xml_accessor :id_value,          :from => "IDValue"
   end
