de16d816818c94cbce75ec9472f431f22e9229b8
Patri <patrimglez@gmail.com>
2014-07-25 18:04:21 +0200

    Update product_identifier

--- lib/onix/product_identifier.rb
+++ lib/onix/product_identifier.rb
@@ -2,11 +2,26 @@
 
 module ONIX
   class ProductIdentifier
-    include ROXML
+    include Virtus.model
 
-    xml_name "ProductIdentifier"
+    attribute :product_id_type, Integer
+    attribute :id_value
 
-    xml_accessor :product_id_type, :from => "ProductIDType", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :id_value, :from => "IDValue"
+    def to_xml
+      ProductIdentifierRepresenter.new(self).to_xml
+    end
+
+    def self.from_xml(data)
+      ProductIdentifierRepresenter.new(self.new).from_xml(data)
+    end
+  end
+
+  class ProductIdentifierRepresenter < Representable::Decorator
+    include Representable::XML
+
+    self.representation_wrap = :ProductIdentifier
+
+    property :product_id_type, as: "ProductIDType", getter: ->(args) { "%02i" % product_id_type unless product_id_type.nil? }
+    property :id_value, as: "IDValue"
   end
 end
