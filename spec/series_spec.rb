e16126777eb4e5da75187043e0bf7bc2a8140864
Patri <patrimglez@gmail.com>
2014-07-25 20:00:01 +0200

    Add specs for access to series IDs

--- spec/series_spec.rb
+++ spec/series_spec.rb
@@ -6,13 +6,13 @@ describe ONIX::Series do
 
   Given(:doc) { load_xml "series.xml" }
 
-  context "should correctly convert to a string" do
+  describe "should correctly convert to a string" do
     Given(:series) { ONIX::Series.from_xml(doc) }
 
     Then{ series.to_xml.to_s.start_with? "<Series>" }
   end
 
-  context "should provide read access to first level attributes" do
+  describe "should provide read access to first level attributes" do
     Given(:series) { ONIX::Series.from_xml(doc) }
 
     Then{  series.title_of_series == "Citizens and Their Governments" }
@@ -23,6 +23,24 @@ describe ONIX::Series do
     describe :title_of_series= do
       When { series.title_of_series = "Cool Science Careers" }
       Then { series.to_xml.to_s.include? "<TitleOfSeries>Cool Science Careers</TitleOfSeries>" }
+    end
+  end
+
+  describe "should provide read access to series IDs" do
+    Given(:series) { ONIX::Series.from_xml(doc) }
+    Then { series.series_identifiers.size == 2 }
+  end
+
+  context "should provide write access to series IDs" do
+    Given(:series_identifier1) { ONIX::SeriesIdentifier.new(series_id_type: 1, id_value: 10001) }
+    Given(:series_identifier2) { ONIX::SeriesIdentifier.new(series_id_type: 2, id_value: 20002) }
+    Given(:series) { ONIX::Series.new }
+
+    describe :series_identifiers= do
+      When { series.series_identifiers = [series_identifier1, series_identifier2] }
+
+      Then { series.to_xml.to_s.include? "<SeriesIDType>01</SeriesIDType>" }
+      Then { series.to_xml.to_s.include? "<IDValue>20002</IDValue>" }
     end
   end
 
