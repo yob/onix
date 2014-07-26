6af055822db356c29a2989739b0451a5ee067b26
Patri <patrimglez@gmail.com>
2014-07-26 19:43:36 +0200

    Update subject

--- lib/onix/subject.rb
+++ lib/onix/subject.rb
@@ -2,14 +2,32 @@
 
 module ONIX
   class Subject
-    include ROXML
+    include Virtus.model
 
-    xml_name "Subject"
+    attribute :subject_scheme_id, Integer
+    attribute :subject_scheme_name
+    attribute :subject_scheme_version
+    attribute :subject_code
+    attribute :subject_heading_text
 
-    xml_accessor :subject_scheme_id,      :from => "SubjectSchemeIdentifier", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :subject_scheme_name,    :from => "SubjectSchemeName"
-    xml_accessor :subject_scheme_version, :from => "SubjectSchemeVersion"
-    xml_accessor :subject_code,           :from => "SubjectCode"
-    xml_accessor :subject_heading_text,   :from => "SubjectHeadingText"
+    def to_xml
+      SubjectRepresenter.new(self).to_xml
+    end
+
+    def self.from_xml(data)
+      SubjectRepresenter.new(self.new).from_xml(data)
+    end
+  end
+
+  class SubjectRepresenter < Representable::Decorator
+    include Representable::XML
+
+    self.representation_wrap = :Subject
+
+    property :subject_scheme_id, as: "SubjectSchemeIdentifier", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    property :subject_scheme_name, as: "SubjectSchemeName"
+    property :subject_scheme_version, as: "SubjectSchemeVersion"
+    property :subject_code, as: "SubjectCode"
+    property :subject_heading_text, as: "SubjectHeadingText"
   end
 end
