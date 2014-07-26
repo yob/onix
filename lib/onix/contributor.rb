9ea53ca744745a299e60e08acb17e81cf6ab0fa0
Patri <patrimglez@gmail.com>
2014-07-26 19:29:31 +0200

    Update contributor

--- lib/onix/contributor.rb
+++ lib/onix/contributor.rb
@@ -2,25 +2,54 @@
 
 module ONIX
   class Contributor
-    include ROXML
+    include Virtus.model
 
-    xml_name "Contributor"
+    attribute :sequence_number, Integer
+    attribute :contributor_role
+    attribute :language_code
+    attribute :sequence_number_within_role, Integer
+    attribute :person_name
+    attribute :person_name_inverted
+    attribute :titles_before_names
+    attribute :names_before_key
+    attribute :prefix_to_key
+    attribute :key_names
+    attribute :names_after_key
+    attribute :suffix_to_key
+    attribute :letters_after_names
+    attribute :titles_after_names
+    attribute :corporate_name
+    attribute :biographical_note
 
-    xml_accessor :sequence_number,      :from => "SequenceNumber", :as => Fixnum
-    xml_accessor :contributor_role,     :from => "ContributorRole"
-    xml_accessor :language_code,        :from => "LanguageCode"
-    xml_accessor :sequence_number_within_role,  :from => "SequenceNumberWithinRole", :as => Fixnum
-    xml_accessor :person_name,          :from => "PersonName"
-    xml_accessor :person_name_inverted, :from => "PersonNameInverted"
-    xml_accessor :titles_before_names,  :from => "TitlesBeforeNames"
-    xml_accessor :names_before_key,     :from => "NamesBeforeKey"
-    xml_accessor :prefix_to_key,        :from => "PrefixToKey"
-    xml_accessor :key_names,            :from => "KeyNames"
-    xml_accessor :names_after_key,      :from => "NamesArterKey"
-    xml_accessor :suffix_to_key,        :from => "SuffixToKey"
-    xml_accessor :letters_after_names,  :from => "LettersAfterNames"
-    xml_accessor :titles_after_names,   :from => "TitlesAfterNames"
-    xml_accessor :corporate_name,       :from => "CorporateName"
-    xml_accessor :biographical_note,    :from => "BiographicalNote"
+    def to_xml
+      ContributorRepresenter.new(self).to_xml
+    end
+
+    def self.from_xml(data)
+      ContributorRepresenter.new(self.new).from_xml(data)
+    end
+  end
+
+class ContributorRepresenter < Representable::Decorator
+    include Representable::XML
+
+    self.representation_wrap = :Contributor
+
+    property :sequence_number, as: "SequenceNumber"
+    property :contributor_role, as: "ContributorRole"
+    property :language_code, as: "LanguageCode"
+    property :sequence_number_within_role, as: "SequenceNumberWithinRole"
+    property :person_name, as: "PersonName"
+    property :person_name_inverted, as: "PersonNameInverted"
+    property :titles_before_names, as: "TitlesBeforeNames"
+    property :names_before_key, as: "NamesBeforeKey"
+    property :prefix_to_key, as: "PrefixToKey"
+    property :key_names, as: "KeyNames"
+    property :names_after_key, as: "NamesArterKey"
+    property :suffix_to_key, as: "SuffixToKey"
+    property :letters_after_names, as: "LettersAfterNames"
+    property :titles_after_names, as: "TitlesAfterNames"
+    property :corporate_name, as: "CorporateName"
+    property :biographical_note, as: "BiographicalNote"
   end
 end
