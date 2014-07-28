72c6702cfb8cbaf97deec9747fb5752748fde3f9
Patri <patrimglez@gmail.com>
2014-07-28 18:32:16 +0200

    Update supply_detail

--- lib/onix/supply_detail.rb
+++ lib/onix/supply_detail.rb
@@ -2,30 +2,52 @@
 
 module ONIX
   class SupplyDetail
-    include ROXML
+    include Virtus.model
 
-    xml_name "SupplyDetail"
+    attribute :supplier_ean_location_number
+    attribute :supplier_san
+    attribute :supplier_name
+    attribute :telephone_number
+    attribute :fax_number
+    attribute :email_address
+    attribute :websites, Array[ONIX::Website]
+    attribute :supplier_role, Integer
+    attribute :supply_to_country
+    attribute :supply_to_territory
+    attribute :availability_status_code, Integer
+    attribute :product_availability, Integer
+    attribute :stock, Array[ONIX::Stock]
+    attribute :pack_quantity, Integer
+    attribute :prices, Array[ONIX::Price]
 
-    xml_accessor :supplier_ean_location_number, :from => "SupplierEANLocationNumber"
-    xml_accessor :supplier_san, :from => "SupplierSAN"
-    xml_accessor :supplier_name, :from => "SupplierName"
-    xml_accessor :telephone_number, :from => "TelephoneNumber"
-    xml_accessor :fax_number, :from => "FaxNumber"
-    xml_accessor :email_address, :from => "EmailAddress"
-    xml_accessor :websites, :from => "Website", :as => [ONIX::Website]
-    xml_accessor :supplier_role, :from => "SupplierRole", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :supply_to_country, :from => "SupplyToCountry"
-    xml_accessor :supply_to_territory, :from => "SupplyToTerritory"
-    xml_accessor :availability_status_code, :from => "AvailabilityStatusCode", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :product_availability, :from => "ProductAvailability", :as => Fixnum, :to_xml => ONIX::Formatters.two_digit
-    xml_accessor :stock, :from => "Stock", :as => [ONIX::Stock]
-    xml_accessor :pack_quantity, :from => "PackQuantity", :as => Fixnum
-    xml_accessor :prices, :from => "Price", :as => [ONIX::Price]
-
-    def initialize
-      self.websites = []
-      self.stock = []
-      self.prices = []
+    def to_xml
+      SupplyDetailRepresenter.new(self).to_xml
     end
+
+    def self.from_xml(data)
+      SupplyDetailRepresenter.new(self.new).from_xml(data)
+    end
+  end
+
+  class SupplyDetailRepresenter < Representable::Decorator
+    include Representable::XML
+
+    self.representation_wrap = :SupplyDetail
+
+    property :supplier_ean_location_number, as: "SupplierEANLocationNumber"
+    property :supplier_san, as: "SupplierSAN"
+    property :supplier_name, as: "SupplierName"
+    property :telephone_number, as: "TelephoneNumber"
+    property :fax_number, as: "FaxNumber"
+    property :email_address, as: "EmailAddress"
+    collection :websites, as: "Website", extend: ONIX::WebsiteRepresenter, class: ONIX::Website
+    property :supplier_role, as: "SupplierRole", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    property :supply_to_country, as: "SupplyToCountry"
+    property :supply_to_territory, as: "SupplyToTerritory"
+    property :availability_status_code, as: "AvailabilityStatusCode", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    property :product_availability, as: "ProductAvailability", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    collection :stock, as: "Stock", extend: ONIX::StockRepresenter, class: ONIX::Stock
+    property :pack_quantity, as: "PackQuantity"
+    collection :prices, as: "Price", extend: ONIX::PriceRepresenter, class: ONIX::Price
   end
 end
