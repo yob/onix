e3a5c16e4396d49b137fd9b98e039eba9d63dbc9
Patri <patrimglez@gmail.com>
2014-07-30 17:51:21 +0200

    Added new Representer with support for short tags to Imprint and new specs for this

--- lib/onix/imprint.rb
+++ lib/onix/imprint.rb
@@ -9,12 +9,12 @@ module ONIX
     attribute :name_code_value
     attribute :imprint_name
 
-    def to_xml
-      ImprintRepresenter.new(self).to_xml
+    def to_xml(type = "reference")
+      type == "reference" ? ImprintRepresenter.new(self).to_xml : ImprintShortRepresenter.new(self).to_xml
     end
 
     def self.from_xml(data)
-      ImprintRepresenter.new(self.new).from_xml(data)
+      data.include?("<Imprint>") ? ImprintRepresenter.new(self.new).from_xml(data) : ImprintShortRepresenter.new(self.new).from_xml(data)
     end
   end
 
@@ -27,5 +27,16 @@ module ONIX
     property :name_code_type_name, as: "NameCodeTypeName"
     property :name_code_value, as: "NameCodeValue"
     property :imprint_name, as: "ImprintName"
+  end
+
+  class ImprintShortRepresenter < Representable::Decorator
+    include Representable::XML
+
+    self.representation_wrap = :imprint
+
+    property :name_code_type, as: "b241", render_filter: ::ONIX::Formatters::TWO_DIGITS
+    property :name_code_type_name, as: "b242"
+    property :name_code_value, as: "b243"
+    property :imprint_name, as: "b079"
   end
 end
