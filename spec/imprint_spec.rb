10877126080f55fcc72f1043c307d715bcd8b25b
Patri <patrimglez@gmail.com>
2014-07-31 12:35:21 +0200

    :scissors: representer for short tags, only reference tags

--- spec/imprint_spec.rb
+++ spec/imprint_spec.rb
@@ -29,31 +29,3 @@ describe ONIX::Imprint do
   end
 
 end
-
-describe ONIX::Imprint, "short tags" do
-
-  Given(:doc) { load_xml "imprint_short_tags.xml" }
-
-  describe "should correctly convert to a string" do
-    Given(:imp) { ONIX::Imprint.from_xml(doc) }
-    Then { imp.to_xml("short").to_s.start_with? "<imprint>" }
-  end
-
-  describe "should provide read access to first level attributes" do
-    Given(:imp) { ONIX::Imprint.from_xml(doc) }
-    Then { imp.imprint_name == "Oxford University Press UK" }
-  end
-
-  context "should provide write access to first level attributes" do
-    Given(:imp) { ONIX::Imprint.new }
-    describe :imprint_name= do
-      When { imp.imprint_name = "Paulist Press" }
-      Then { imp.to_xml("short").to_s.include? "<b079>Paulist Press</b079>" }
-    end
-    describe :name_code_type= do
-      When { imp.name_code_type = 1 }
-      Then { imp.to_xml("short").to_s.include? "<b241>01</b241>" }
-    end
-  end
-
-end
